fa04a326c345b05e64eef9bfacf80fcac639d074
Sujan Amatya <sujzn.amatya@gmail.com>
2019-03-07 19:18:36 +1100

    Final script of logic question

--- logic.php
+++ logic.php
@@ -1,7 +1,16 @@
 <?php
 
 	for($i =1; $i<=100; $i++){
-		echo '&nbsp'.$i.',';
+		//echo '&nbsp'.$i.',';
+		if(($i % 3 == 0) && ($i % 5 == 0)){
+			echo '&nbsp foobar,';
+		} elseif ($i % 5 == 0) {
+			echo '&nbsp bar,';
+		} elseif ($i % 3 == 0) {
+			echo '&nbsp foo,';	
+		} else {
+			echo '&nbsp'.$i.',';
+		}
 	}
 
 ?>
